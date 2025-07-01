c152e98ef76e0e8fff6be50c105edb1ab4c02e0e
An Nguyen <annguyen@Ans-MacBook-Air.local>
2025-06-30 23:54:32 -0500

    Finished up dashboards, need to add to docker

--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js"
+import productRoutes from "./routes/product.js"
+import transactionRoutes from "./routes/transaction.js"
+import Product from "./models/Product.js"
 import KPI from "./models/KPI.js"
-import { kpis } from "./data/data.js"
+import { kpis, products, transactions } from "./data/data.js" 
+import Transaction from "./models/Transaction.js"
 
 /* CONFIGS */
 dotenv.config();
@@ -24,6 +28,8 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
 mongoose
@@ -32,8 +38,10 @@ mongoose
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
         /* ADD DATA ONE TIME ONLY OR AS NEEDED*/
-        await mongoose.connection.db.dropDatabase();
-        KPI.insertMany(kpis);
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis);
+        // Product.insertMany(products);
+        // Transaction.insertMany(transactions);
     })
     .catch((error) => console.log(`${error} did not connect`));
 
